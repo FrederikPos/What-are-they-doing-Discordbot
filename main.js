481143972d1e1cef2e855f839bc16b413b6ae21c
FrederikPosTheDev <57841098+FrederikPos@users.noreply.github.com>
2020-12-23 19:28:32 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+const prefix = '.';
+
+const fs = require('fs');
+
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+for(const file of commandFiles){
+    const command = require('./commands/'${file});
+
+    client.commands.set(command.name, command);
+}
+
+
+    client.once('ready', () => {
+    console.log('Bot is online!');
+});
+
+client.on('message', message =>{
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if(command === 'checkstatus')
+        client.commands.get('checkstatus').execute(message, args);
+    
+        message.channel.send('I will check the status of that person'); 
+    }
+,); 
+
+    client.login('NzkxMzQ1MTAxMjk2NTAwNzQ3.X-Nzjg.EckofOpd0Z45L6_U4SNEArpus_4');
